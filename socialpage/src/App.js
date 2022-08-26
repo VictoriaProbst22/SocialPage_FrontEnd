8833c6ac1704a48778e258499e4c21a65b7ad413
Victoria Probst <victoria.probst13@gmail.com>
2022-08-26 14:07:20 -0400

    Started REACT Frontend

--- socialpage/src/App.js
+++ socialpage/src/App.js
@@ -1,8 +1,41 @@
 
+//import pages
+import Navbar from "./NavBar";
+import DisplayPageData from "./DisplayPage";
+
+//import technologies 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
+
 function App() {
+
+  const [page, setPage] = useState([])
+
+
+
+
+  const getPageData = async() =>{
+    try {
+      let response = await axios.get(`http://127.0.0.1:8000/page/`);
+      setPage(response.data)
+  
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  
+  useEffect(()=>{
+    getPageData();
+  }, [])
+
+
+
   return (
     <div>
-      Hello World!
+      <Navbar/>
     </div>
   );
 }
---
+++ socialpage/src/DisplayPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DisplayPageData = ({pageData}) => {
+
+
+    
+
+    return ( <div>
+
+    </div> );
+}
+ 
+export default DisplayPageData;
