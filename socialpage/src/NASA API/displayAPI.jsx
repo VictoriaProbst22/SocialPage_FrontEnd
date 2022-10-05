3c3dbbbbd5e9663f6aa2e16d83042c1f9b16fcb7
Victoria Probst <victoria.probst13@gmail.com>
2022-10-05 19:27:35 -0400

    Fixed DisplayAPI File bug

--- socialpage/src/NASA API/displayAPI.jsx
+++ socialpage/src/NASA API/displayAPI.jsx
@@ -6,22 +6,6 @@ const DisplayApi = (props) => {
     console.log('Picture Results: ', props.pictureResults);
 
     
-    const addBook= async (pictureToAdd)=>{
-      let newPicture = {
-          "title": pictureToAdd.title,
-          "date": pictureToAdd.date,
-          "explanation": pictureToAdd.explanation,
-          
-      }
-      console.log(newPicture)
-       try {
-          let response = await axios.post("`https://api.nasa.gov/planetary/apod?api_key=MwXfSmgO8f2fkgnrhdhwjJQSgXFvKkVnrNjHU92S", newPicture) 
-          console.log(response.data)
-      } catch (error) {
-          console.log(error.message)
-      }
-
-  }
     
   //map Results of "Title, Date, Explanation" & Research Media and URL to display
 
